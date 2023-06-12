189737899766171674c89366d229208fb10f379c
Liam <liamjwatson@aol.com>
2023-06-12 14:03:20 +0100

    items being rendered to list

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+];
+
 export default function App() {
   return (
     <div className="app">
@@ -22,13 +27,32 @@ function Form() {
 }
 
 function PackingList() {
-  return <div className="list">List</div>;
+  return (
+    <div className="list">
+      <ul>
+        {/* for each item  render an Item compopent*/}
+        {initialItems.map((item) => (
+          <Item itemObj={item} />
+        ))}
+      </ul>
+    </div>
+  );
 }
-
 function Stats() {
   return (
     <footer className="stats">
       <em>👜 You have X items on your list, and you already packed X (%)</em>
     </footer>
+  );
+}
+
+function Item({ itemObj }) {
+  return (
+    <li>
+      <span style={itemObj.packed ? { textDecoration: "line-through" } : {}}>
+        {itemObj.quantity} {itemObj.description}
+      </span>
+      <button>❌</button>
+    </li>
   );
 }
