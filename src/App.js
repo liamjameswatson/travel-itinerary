e3e15e299f351eff93f21d38738ad85076827381
Liam <liamjwatson@aol.com>
2023-06-12 12:16:00 +0100

    initial commit - basic react app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h1>Travel list</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
